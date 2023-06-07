27a39bebccb0ac97c647ecfdcf274986642fe9cd
opulencedeveloper <victorkudos@gmail.com>
2023-06-07 21:16:43 +0100

    whatsapp url update

--- helpers/whatsapp.js
+++ helpers/whatsapp.js
@@ -1,6 +1,6 @@
 const handleWhatsAppClick = () => {
     const phoneNumber = "+2348184297165";
-    const message = "Hello Opulence, how much is the money-hub website";
+    const message = "Hello Opulence, how much is the real-estate website";
   
     const encodedMessage = encodeURIComponent(message);
     const whatsappUrl = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
